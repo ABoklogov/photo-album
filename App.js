3c2015b6fd92e95acfdbe2bd73a2e52ff0dfc27d
Алексей Боклогов <kashira87@mail.ru>
2022-08-16 15:05:20 +0300

    фиксация изменений

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-// import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import useFonts from './hooks/useFonts';
 import LoginScreen from './screens/LoginScreen';
 import RegistrationScreen from './screens/RegistrationScreen';
+import AppLoading from 'expo-app-loading';
 
+// подключение шрифтов
+const loadFonts = async () => {
+  await useFonts();
+};
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={err => console.warn(err)}
+      />
+    )
+  };
 
   return (
     <View style={styles.container}>
-      {/* <Text style={{ fontFamily: 'gothampro_bold' }}>Привет</Text> */}
-      <LoginScreen />
+      <Text style={{ fontFamily: 'gothampro_bold' }}>Привет</Text>
+      {/* <LoginScreen /> */}
       {/* <RegistrationScreen /> */}
     </View>
   );
@@ -17,9 +34,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: "center",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: "center",
   },
 });
