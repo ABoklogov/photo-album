72827ded8fd01a179f3ac259684e09a4c031e240
Алексей Боклогов <kashira87@mail.ru>
2022-08-16 22:39:23 +0300

    подключает шрифты

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import LoginScreen from './screens/LoginScreen';
 import RegistrationScreen from './screens/RegistrationScreen';
 
+// подключение шрифтов
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'GothamPro-Light': require('./assets/fonts/GotmanPro/GothamPro-Light.ttf'),
+    'GothamPro-Medium': require('./assets/fonts/GotmanPro/GothamPro-Medium.ttf'),
+    'GothamPro-Bold': require('./assets/fonts/GotmanPro/GothamPro-Bold.ttf'),
+    'GothamPro-Black': require('./assets/fonts/GotmanPro/GothamPro-Black.ttf'),
+  })
+};
+
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  // компонент лоадер, пока шрифты подключаются
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={err => console.log(err)}
+      />
+    )
+  }
 
   return (
     <View style={styles.container}>
+      <Text style={{ 'fontFamily': 'GothamPro-Bold' }}>frfregfre</Text>
       {/* <LoginScreen/> */}
-      <RegistrationScreen />
+      {/* <RegistrationScreen /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // justifyContent: "center",
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: 'center',
   },
 });
